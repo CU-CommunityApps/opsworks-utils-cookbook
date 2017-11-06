cf019b12496667a252cb54e0782b6cc4379e7509
nl85cit <ned.lacelle@cornell.edu>
2017-11-06 10:18:06 -0500

    update to be compatable with multiple xNix versions

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -1,1 +1,16 @@
-yum_package 'nano'
+package_install_array =  ['nano', 'emacs']
+
+package_install_array.each do |this_package|
+  case node[:platform]
+  when "amazon"
+    package "#{this_package}"
+  when "redhat"
+    package "#{this_package}"
+  when "ubuntu"
+    package "#{this_package}"
+  when "suse"
+    package "#{this_package}"
+  else
+    package "#{this_package}"
+ end
+end
