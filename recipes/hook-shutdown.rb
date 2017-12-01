672911a0aae1a1083c880d0c9041561e9a35a9d0
Paul Allen <pea1@cornell.edu>
2017-12-01 16:23:08 -0500

    remove cpu credit alarm at shutdown

--- recipes/hook-shutdown.rb
+++ recipes/hook-shutdown.rb
@@ -20,6 +20,10 @@ log 'Removing cloudwatch alarm: status check failed' do
   notifies :delete, 'aws_cloudwatch[status-check-alarm]', :immediately
 end
 
+log 'Removing cloudwatch alarm: cpu credits balance alarm' do
+  notifies :delete, 'aws_cloudwatch[cpu-credits-balance-alarm]', :immediately
+end
+
 #### ROUTE53 ####
 include_recipe 'opsworks-utils-cookbook::dns'
 
