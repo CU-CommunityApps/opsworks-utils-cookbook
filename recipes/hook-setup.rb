59199df2fd820be74c22bc6790a44ef81488c560
Paul Allen <pea1@cornell.edu>
2018-11-08 15:56:53 -0500

    add defaults and guards to cpu-limit and selinux recipes

--- recipes/hook-setup.rb
+++ recipes/hook-setup.rb
@@ -1,5 +1,6 @@
 # Recipe to be run on brand new instances and at instance start (after a stop).
 
+include_recipe 'opsworks-utils-cookbook::se-linux-mode'
 include_recipe 'opsworks-utils-cookbook::papertrail'
 include_recipe 'opsworks-utils-cookbook::instance-tags'
 include_recipe 'opsworks-utils-cookbook::aws-agents'
@@ -7,3 +8,4 @@ include_recipe 'opsworks-utils-cookbook::aws-alarms'
 include_recipe 'opsworks-utils-cookbook::packages'
 include_recipe 'opsworks-utils-cookbook::timezone'
 include_recipe 'opsworks-utils-cookbook::dns-create'
+include_recipe 'opsworks-utils-cookbook::ttype-cpu-limit'
--- recipes/se-linux-mode.rb
+++ recipes/se-linux-mode.rb
@@ -10,12 +10,14 @@ ruby_block 'se-linux-permissive' do
   block do
     file = Chef::Util::FileEdit.new('/etc/selinux/config')
     # Handle a couple different scenarios
-    file.search_file_replace_line('SELINUX=enforcing', 'SELINUX=permissive')
-    file.search_file_replace_line('SELINUX=disabled', 'SELINUX=permissive')
-    file.insert_line_if_no_match('SELINUX=permissive', 'SELINUX=permissive')
+    file.search_file_replace_line('SELINUX=enforcing', "SELINUX=#{node['opsworks-utils']['selinux']['mode']}")
+    file.search_file_replace_line('SELINUX=permissive', "SELINUX=#{node['opsworks-utils']['selinux']['mode']}")
+    file.search_file_replace_line('SELINUX=disabled', "SELINUX=#{node['opsworks-utils']['selinux']['mode']}")
+    file.insert_line_if_no_match("SELINUX=#{node['opsworks-utils']['selinux']['mode']}", "SELINUX=#{node['opsworks-utils']['selinux']['mode']}")
     file.write_file
   end
   notifies :request_reboot, 'reboot[reboot-after-se-linux-change]'
+  only_if { node['opsworks-utils']['selinux']['set-mode'] }
 end
 
 reboot 'reboot-after-se-linux-change' do
